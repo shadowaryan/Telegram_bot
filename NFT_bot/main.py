ef833f116145385330439e26e8e1b846e92d432c
Shadow <borntodevelop77@gmail.com>
2022-02-21 10:44:53 +0530

    message handler add

--- NFT_bot/main.py
+++ NFT_bot/main.py
@@ -4,6 +4,16 @@ from telegram.ext import *
 
 TOKEN = '5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k'
 
+
+def handle_message(update,context):
+    message = update.message.text
+
+    if message in ("Hi","Hii","hii","hello","Hello"):
+        update.message.reply_text(f"{message}, there")
+    else:
+        update.message.reply_text("Invalid Text , use /help ")
+
+    
 def start (update, context):
     update.message.reply_text("""hello,welcome
     For more Commands use - /help""")
@@ -20,6 +30,7 @@ disp = updater.dispatcher
 
 disp.add_handler(CommandHandler("start", start))
 disp.add_handler(CommandHandler("help", help))
+disp.add_handler(MessageHandler(Filters.text, handle_message))
 
 updater.start_polling()
 updater.idle
