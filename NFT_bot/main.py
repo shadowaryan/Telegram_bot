652c410a12827c9b32463a4f405219a75158fc50
Shadow <borntodevelop77@gmail.com>
2022-02-23 13:35:19 +0530

    link command added

--- NFT_bot/main.py
+++ NFT_bot/main.py
@@ -1,10 +1,9 @@
-from telegram import Update
+from telegram import *
+from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext import *
-import os
+from uuid import uuid4
 
 TOKEN = '5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k'
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
 
 
 def handle_message(update,context):
@@ -12,13 +11,13 @@ def handle_message(update,context):
 
     if message in ("Hi","Hii","hii","hello","Hello"):
         update.message.reply_text(f"{message}, there")
-    else:
-        update.message.reply_text("Invalid Text , use /help ")
+   # else:
+    #    update.message.reply_text("Invalid Text , use /help ")
 
     
 def start (update, context):
     update.message.reply_text("""hello,welcome
-    For more Commands use - /help""")
+   For more Commands use - /help""")
 
 def help (update, context):
     update.message.reply_text("""
@@ -26,16 +25,90 @@ def help (update, context):
     /help - to view commands
     """)
 
+def get_link(update, context):
+    value = update.message.text.partition(' ')[2]
+    update.message.reply_text(f"LINK GIVEN BY YOU - {value}")
 
-updater = Updater(TOKEN)
-disp = updater.dispatcher
 
-disp.add_handler(CommandHandler("start", start))
-disp.add_handler(CommandHandler("help", help))
-disp.add_handler(MessageHandler(Filters.text, handle_message))
+'''def put(update, context):
+    """Usage: /put value"""
+    # Generate ID and separate value from command
+    key = str(uuid4())
+    # We don't use context.args here, because the value may contain whitespaces
+    value = update.message.text.partition(' ')[2]
 
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN,
-                      webhook_url="https://<appname>.herokuapp.com/" + TOKEN)
-updater.idle()
+    # Store value
+    context.user_data[key] = value
+    # Send the key to the user
+    update.message.reply_text(key)
+'''
+
+
+def get(update, context):
+    """Usage: /get uuid"""
+    # Seperate ID from command
+    key = context.args[0]
+
+    # Load value and send it to the user
+    value = context.user_data.get(key, 'Not found')
+    update.message.reply_text(value)
+
+
+
+
+if __name__ == '__main__':
+    updater = Updater('5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k', use_context=True)
+    dp = updater.dispatcher
+    print("started")
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("link", get_link))
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+    
+    
+    #dp.add_handler(CommandHandler('put', put))
+    #dp.add_handler(CommandHandler('get', get))
+
+    updater.start_polling()
+    updater.idle()
+
+
+
+
+'''
+
+from uuid import uuid4
+from telegram.ext import Updater, CommandHandler
+
+def put(update, context):
+    """Usage: /put value"""
+    # Generate ID and separate value from command
+    key = str(uuid4())
+    # We don't use context.args here, because the value may contain whitespaces
+    value = update.message.text.partition(' ')[2]
+
+    # Store value
+    context.user_data[key] = value
+    # Send the key to the user
+    update.message.reply_text(key)
+
+def get(update, context):
+    """Usage: /get uuid"""
+    # Seperate ID from command
+    key = context.args[0]
+
+    # Load value and send it to the user
+    value = context.user_data.get(key, 'Not found')
+    update.message.reply_text(value)
+
+if __name__ == '__main__':
+    updater = Updater('5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k', use_context=True)
+    dp = updater.dispatcher
+    print("sad") 
+    dp.add_handler(CommandHandler('put', put))
+    dp.add_handler(CommandHandler('get', get))
+
+    updater.start_polling()
+    updater.idle()
+
+   '''
