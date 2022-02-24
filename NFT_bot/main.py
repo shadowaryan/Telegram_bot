2ef4fb03e1d6df8a12211619e8b357f3fbb90669
Shadow <borntodevelop77@gmail.com>
2022-02-24 15:28:56 +0530

    models updated

--- NFT_bot/main.py
+++ NFT_bot/main.py
@@ -6,7 +6,6 @@ from models import *
 from sqlalchemy.orm import *
 import requests
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://spqqojmysvclhl:35e13032f8326f8b7908e52a75e65215a62437d5c0c618aaee8a14392405e188@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d7jch8clhgaktb', echo=True)
 
@@ -21,7 +20,8 @@ TOKEN = '5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k'
 #message handling command
 def handle_message(update,context):
     message = update.message.text
-
+    print(update.effective_user.id)
+    print(update.effective_user.username)
     if message in ("Hi","Hii","hii","hello","Hello"):
         update.message.reply_text(f"{message}, there")
     else:
@@ -38,37 +38,43 @@ def help (update, context):
     update.message.reply_text("""
     Commands:
     /help - to view commands
-    /link - to give us the link to store 
+    /collection - write the nft collection name 
     """)
 
 #chat id function
 def chat_id_(update, context):
-    id_value=update.message.chat_id
+    id_value=update.effective_user.id
     return id_value
 
+def username_id(update, context):
+    username_value=update.effective_user.username
+    return username_value
 
 #get link bot command
-def get_link(update, context):
+def collection_name(update, context):
     value = update.message.text.partition(' ')[2]
     update.message.reply_text(f"LINK GIVEN BY YOU - {value}")
     response = requests.get(value)
     
     if response.status_code == True:
 
-        main_id = chat_id_(update, context)
+        main_chat_id = chat_id_(update, context)
+        new_chat_id = User(chat_id=main_chat_id)
+        db_chat_id = session.query(User).filter_by(chat_id=main_chat_id).first()
+        if new_chat_id not in db_chat_id:
+            session.add(new_chat_id)
 
-        ed_user = User(username=main_id)
-        our_user = session.query(User).filter_by(name=main_id).first()
-    
-        if ed_user not in our_user:
-            session.add(ed_user)
+        main_username_id = username_id(update, context)
+        new_username_id = User(username=main_username_id)
+        db_username_id = session.query(User).filter_by(username=main_username_id).first()
+        if new_username_id not in db_username_id:
+            session.add(new_username_id)
 
-        ed_collection = Collection(Collection_link=value)
-    
-        our_collection = session.query(Collection).filter_by(Collection_link=value).first()
-    
-        if ed_collection not in our_collection:
-            session.add(ed_collection)
+        
+        new_collection = Collection(slug=value)
+        db_collection = session.query(Collection).filter_by(slug=value).first()
+        if new_collection not in db_collection:
+            session.add(new_collection)
 
         #for row in session.query(User, User.username).all():
         #    print(row.User, row.username)
@@ -136,7 +142,7 @@ if __name__ == '__main__':
     print("started")
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("link", get_link))
+    dp.add_handler(CommandHandler("collection", collection_name))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     
     
--- NFT_bot/models.py
+++ NFT_bot/models.py
@@ -1,5 +1,32 @@
 from sqlalchemy import *
-from sqlalchemy.orm import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    username = Column(String(50)) #shadowaryan 
+    chat_id = Column(Integer) #123456789
+    collections = relationship('Collection',secondary = 'user_collection', back_populates='user')
+
+class Collection(Base):
+    __tablename__ = 'collection'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    slug = Column(String(512))
+    users = relationship('User',secondary = 'user_collection', back_populates='collection')
+
+
+class User_Collection(Base):
+    __tablename__ = 'user_collection'
+    user_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    collection_id = Column(Integer, ForeignKey('collection.id'),primary_key=True)
+
+print('hii')
+'''
+from sqlalchemy import *
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -8,16 +35,18 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     username = Column(String(50))
-    collections = relationship('Collection',secondary = 'link', back_populates='user')
+    collections = relationship('Collection',secondary = 'user_collection', back_populates='user')
 
 class Collection(Base):
     __tablename__ = 'collection'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    collection_link = Column(String(512))
-    users = relationship('User',secondary = 'link', back_populates='collection')
+    name = Column(String(512))
+    #slug = 
+    users = relationship('User',secondary = 'user_collection', back_populates='collection')
 
 
-class Link(Base):
-    __tablename__ = 'link'
+class User_collection(Base):
+    __tablename__ = 'user_collection'
     user_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
     collection_id = Column(Integer, ForeignKey('collection.id'),primary_key=True)
+'''
