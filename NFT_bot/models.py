ea446103f8767e73aace7c3e6690f7c8336b5a6b
Shadow <borntodevelop77@gmail.com>
2022-02-24 14:19:10 +0530

    updates

--- NFT_bot/models.py
+++ NFT_bot/models.py
@@ -5,19 +5,19 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     username = Column(String(50))
-    collections = relationship('Collection', secondary = 'link')
+    collections = relationship('Collection',secondary = 'link', back_populates='user')
 
 class Collection(Base):
     __tablename__ = 'collection'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    Collection_link = Column(String(512))
-    users = relationship('User', secondary = 'link')
+    collection_link = Column(String(512))
+    users = relationship('User',secondary = 'link', back_populates='collection')
 
 
 class Link(Base):
     __tablename__ = 'link'
-    user_id = Column(Integer, ForeignKey('User.id'),primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
     collection_id = Column(Integer, ForeignKey('collection.id'),primary_key=True)
