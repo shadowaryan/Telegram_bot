78b5264911a1ef81d4c70e05dfeb49c4175235c3
Shadow <borntodevelop77@gmail.com>
2022-02-24 21:19:16 +0530

    NFT floor price added

--- NFT_bot/models.py
+++ NFT_bot/models.py
@@ -9,44 +9,17 @@ class User(Base):
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     username = Column(String(50)) #shadowaryan 
     chat_id = Column(Integer) #123456789
-    collections = relationship('Collection',secondary = 'user_collection', back_populates='user')
+    collection = relationship('Collection',secondary = 'user_collection', back_populates='user')
 
 class Collection(Base):
     __tablename__ = 'collection'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     slug = Column(String(512))
-    users = relationship('User',secondary = 'user_collection', back_populates='collection')
+    floor_price = Column(Float(10,5))
+    user = relationship('User',secondary = 'user_collection', back_populates='collection')
 
 
 class User_Collection(Base):
     __tablename__ = 'user_collection'
     user_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
     collection_id = Column(Integer, ForeignKey('collection.id'),primary_key=True)
-
-print('hii')
-'''
-from sqlalchemy import *
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    username = Column(String(50))
-    collections = relationship('Collection',secondary = 'user_collection', back_populates='user')
-
-class Collection(Base):
-    __tablename__ = 'collection'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    name = Column(String(512))
-    #slug = 
-    users = relationship('User',secondary = 'user_collection', back_populates='collection')
-
-
-class User_collection(Base):
-    __tablename__ = 'user_collection'
-    user_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
-    collection_id = Column(Integer, ForeignKey('collection.id'),primary_key=True)
-'''
