53c10cf5e379a67571b41aa2c94f934ecf1e938d
Shadow <borntodevelop77@gmail.com>
2022-02-24 13:35:56 +0530

    session updated

--- NFT_bot/main.py
+++ NFT_bot/main.py
@@ -7,12 +7,14 @@ from sqlalchemy.orm import *
 import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('postgres://spqqojmysvclhl:35e13032f8326f8b7908e52a75e65215a62437d5c0c618aaee8a14392405e188@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d7jch8clhgaktb', echo=True)
+
 Session = sessionmaker(bind=engine)
-Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
-engine = create_engine('postgres://spqqojmysvclhl:35e13032f8326f8b7908e52a75e65215a62437d5c0c618aaee8a14392405e188@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d7jch8clhgaktb', echo=True)
+
 
 TOKEN = '5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k'
 
