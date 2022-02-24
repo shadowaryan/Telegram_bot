402ab6e72a1f8275ab317a18cfbfbda441950f17
Sirri69 <pranav2748@gmail.com>
2022-02-25 01:08:04 +0530

    Setup add collection

--- NFT_bot/main.py
+++ NFT_bot/main.py
@@ -1,5 +1,3 @@
-from urllib import response
-from flask import Response
 from telegram import *
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext import *
@@ -10,6 +8,23 @@ import requests
 from sqlalchemy import create_engine
 from sqlalchemy.sql import exists
 import json
+
+# from utils import get_collection_id
+
+def get_collection_id(slug):
+    collection = session.query(Collection).filter_by(slug=slug).first()
+    if collection:
+        return collection.id
+    else:
+        floor_price = requests.get(f'https://api.opensea.io/collection/{slug}/stats').json()['stats']['floor_price']
+        collection = Collection(slug=slug, floor_price=floor_price)
+        session.add(collection)
+        session.commit()
+        return collection.id
+
+
+
+
 
 engine = create_engine('postgresql://spqqojmysvclhl:35e13032f8326f8b7908e52a75e65215a62437d5c0c618aaee8a14392405e188@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d7jch8clhgaktb', echo=False)
 
@@ -23,7 +38,7 @@ TOKEN = '5221341356:AAF5D4OKX3rEHv5M3KvyY6Sg9caipj0ej-k'
 
 
 #bot start command    
-def start (update, context):
+def start(update, context):
 
     update.message.reply_text("""hello,welcome
     For more Commands use - /help""")
@@ -51,27 +66,28 @@ def help (update, context):
 
         
 #bot command to get collection data 
-def collection_name(update, context):
+def add_collection(update, context):
     url = update.message.text.partition(' ')[2]
     slug = url.split('/')[-1]
-    update.message.reply_text(f"NFT Collection Name - {slug}")
-    res = f'https://api.opensea.io/collection/{slug}/stats'
-    response = requests.get(res)
-    data = response.json()
-    floor_price = data['stats']['floor_price']
+    # update.message.reply_text(f"NFT Collection Name - {slug}")
+    # res = f'https://api.opensea.io/collection/{slug}/stats'
+    # response = requests.get(res)
+    # data = response.json()
+    # floor_price = data['stats']['floor_price']
     
-    if response.status_code == 200:
-        
-        collection = Collection(floor_price=floor_price,slug=slug)
-        # db_collection = session.query(Collection).filter_by(slug=slug).first()
-        
-        if not session.query(session.query(Collection).filter_by(slug=slug).exists()).scalar():
-            session.add(collection)
-        session.commit()
+    # if response.status_code == 200:
+    user = session.query(User).filter_by(chat_id=update.effective_user.id).first()
+    collection_id = get_collection_id(slug)
+    # db_collection = session.query(Collection).filter_by(slug=slug).first()
+    
+    if not session.query(session.query(Collection, User).filter(User_Collection.collection_id==collection_id, User_Collection.user_id==user.id).exists()).scalar():
+        user.collection.append(session.query(Collection).filter_by(id=collection_id).first())
+        # session.add()
+    session.commit()
 
 
-    else:
-        update.message.reply_text('invalid input')
+    # else:
+    #     update.message.reply_text('invalid input')
    
 
 #message handling command
@@ -91,7 +107,7 @@ if __name__ == '__main__':
     print("started")
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("add_collection", collection_name))
+    dp.add_handler(CommandHandler("add_collection", add_collection))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     
 
---
+++ NFT_bot/utils.py
@@ -0,0 +1,15 @@
+from models import *
+from .main import session
+
+import requests
+
+def get_collection_id(slug):
+    collection = session.query(Collection).filter_by(slug=slug).first()
+    if collection:
+        return collection.id
+    else:
+        floor_price = requests.get(f'https://api.opensea.io/collection/{slug}/stats').json()['stats']['floor_price']
+        collection = Collection(slug=slug, floor_price=floor_price)
+        session.add(collection)
+        session.commit()
+        return collection.id
